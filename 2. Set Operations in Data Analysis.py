a17a8bb08b1f675a21891948ceaeb9ed2ef2d9f4
jlake70 <jlake7021@gmail.com>
2024-07-23 12:39:02 -0400

    finishing question 2

---
+++ 2. Set Operations in Data Analysis.py
@@ -0,0 +1,21 @@
+
+customer_ids = ["C001", "C002", "C003", "C002", "C001", "C004"]
+
+customer_ids.remove("C002")
+customer_ids.remove("C001")
+
+print(customer_ids)
+
+
+
+
+
+#def delete_ids(ids):
+    #customer_ids = set(ids)
+    #customer_ids_list = list(customer_ids)
+
+    #return customer_ids_list
+#unique_customer_ids = customer_ids.remove()
+#print(unique_customer_ids)
+
+#delete_ids(ids)
